e1cf019f5eda82237617fcc8c41962024367e4f9
Flávio Renato <peubevbr@gmail.com>
2022-09-22 20:14:16 -0300

    Finalização do projeto

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function toggleMenu() {
+    const menuToggle = document.querySelector(".menuToggle");
+    const navigation = document.querySelector(".navigation");
+    menuToggle.classList.toggle("active"); 
+    navigation.classList.toggle("active");
+    /* toggle => adiciona a clase active, caso ela já exista então remove a classe */
+};
